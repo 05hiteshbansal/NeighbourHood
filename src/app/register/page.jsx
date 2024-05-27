8f4d691096acae7fce6991f01f0e1bb692d61999
Hitesh_Bansal <h.hiteshbansal@gmail.com>
2024-05-27 21:25:42 +0530

    changes

--- src/app/register/page.jsx
+++ src/app/register/page.jsx
@@ -6,7 +6,7 @@ const Register = () => {
     <section class="text-gray-600 body-font bg-black">
   <div class="container px-5 py-5 mx-auto flex flex-wrap items-center">
     <div class="w-3/5 max-h-3/5 ">
-      <Image src={i1} className=' w-full h-full object-cover' />
+      <Image src={i1} className=' w-full h-full object-cover' alt='' />
     </div>
     <div class="lg:w-2/6 md:w-1/2 bg-gray-100 rounded-lg p-8 flex flex-col md:ml-auto w-full mt-10 md:mt-0">
       <h2 class="text-gray-900 text-lg font-medium title-font mb-5">Sign Up</h2>
@@ -19,7 +19,7 @@ const Register = () => {
         <input type="email" id="email" name="email" class="w-full bg-white rounded border border-gray-300 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"/>
       </div>
       <button class="text-white bg-indigo-500 border-0 py-2 px-8 focus:outline-none hover:bg-indigo-600 rounded text-lg">Button</button>
-      <p class="text-xs text-gray-500 mt-3">Literally you probably haven't heard of them jean shorts.</p>
+      <p class="text-xs text-gray-500 mt-3">Literally you probably have not heard of them jean shorts.</p>
     </div>
   </div>
 </section>
